ef610fe65b25f4d2afed037d89c8c81b0b95d755
Somen Singha <somensingha06@gmail.com>
2022-05-05 23:24:30 +0600

    post added

--- index.js
+++ index.js
@@ -41,12 +41,12 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/inventory/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await inventoryCollection.deleteOne(query);
-      res.send(result);
-    });
+    // app.delete("/inventory/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await inventoryCollection.deleteOne(query);
+    //   res.send(result);
+    // });
   } finally {
   }
 }
